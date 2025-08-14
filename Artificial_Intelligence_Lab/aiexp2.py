41f410b78fb6f2651a52b4fba9f7a4498ea73f76
karanjagtap29 <karan.jagtap24@mmit.edu.in>
2025-08-14 09:47:39 +0530

    it's create

---
+++ Artificial_Intelligence_Lab/aiexp2.py
@@ -0,0 +1,25 @@
+sub1=str(input("enter subject 1"))
+sub2=str(input("enter subject 2"))
+
+if(sub1 == "Maths" and sub2 == "Physics") :
+      print("mechanical engineering")
+
+elif(sub1== "Programming" and Sub2 == "Maths") :
+      print("Computer Engineering")
+
+elif(sub1 == "Biology" and sub2 == "Chemistry") :
+      print("Biotechnology")
+
+elif(sub1 == "Circuits" and sub2 == "Maths") :
+      print("Electronic Engineering")
+
+elif(sub1 == "Programming" and sub2 == "Statistics") :
+      print("Artificial Intelligence and Data Science")
+
+elif(sub1 == "Programming" and sub2 == "AI Concepts") :
+      print("Artificial Intelligence and Machine Learning Engineering")
+
+else:
+      print("invalid")
+
+
